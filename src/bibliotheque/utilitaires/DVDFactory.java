c9417f1d3dc4c93da9b73e0d568a16e02233bfc7
Ozan Genc <ozan.genc@condorcet.be>
2024-04-02 18:26:11 +0200

    Fin des TODO

--- src/bibliotheque/utilitaires/DVDFactory.java
+++ src/bibliotheque/utilitaires/DVDFactory.java
@@ -10,26 +10,38 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DVDFactory extends OuvrageFactory{
-    public Ouvrage addDetail(String titre, int ageMin, LocalDate dateParution, double prixLocation, String langue, String genre){
+    public Ouvrage addDetail(String titre, int ageMin, LocalDate dateParution, double prixLocation, String langue, String genre) {
         System.out.println("code : ");
-        long code= sc.nextLong();
-        LocalTime dureeTotale=Utilitaire.lecTime();
-        byte nbreBonus= sc.nextByte();
-        DVD dvd =new DVD(titre,ageMin,dateParution,prixLocation,langue,genre,code,dureeTotale,nbreBonus);
+        long code = sc.nextLong();
+        LocalTime dureeTotale = Utilitaire.lecTime();
+        byte nbreBonus = sc.nextByte();
+        DVD dvd = new DVD(titre, ageMin, dateParution, prixLocation, langue, genre, code, dureeTotale, nbreBonus);
         System.out.println("autres langues");
-        List<String> langues = new ArrayList<>(Arrays.asList("anglais","français","italien","allemand","fin"));
+        List<String> langues = new ArrayList<>(Arrays.asList("anglais", "français", "italien", "allemand", "fin"));
         int choix;
-        do{
-            choix=Utilitaire.choixListe(langues);
-            if(choix==langues.size())break;
-            dvd.getAutresLangues().add(langues.get(choix-1));//TODO vérifier unicité ou utiliser set et pas de doublon avec langue d'origine
-        }while(true);
+        //TODO vérifier unicité ou utiliser set et pas de doublon avec langue d'origine
+        do {
+            choix = Utilitaire.choixListe(langues);
+            if (choix == langues.size()) break;
+            String langueChoisie = langues.get(choix - 1);
+            if (!dvd.getAutresLangues().contains(langueChoisie)) {
+                dvd.getAutresLangues().add(langueChoisie);
+            } else {
+                System.out.println("Cette langue est déjà sélectionnée.");
+            }
+        } while (true);
         System.out.println("sous-titres");
-        do{
-            choix=Utilitaire.choixListe(langues);
-            if(choix==langues.size())break;
-            dvd.getSousTitres().add(langues.get(choix-1));//TODO vérifier unicité ou utiliser set
-        }while(true);
+        //TODO vérifier unicité ou utiliser set
+        do {
+            choix = Utilitaire.choixListe(langues);
+            if (choix == langues.size()) break;
+            String langueChoisie = langues.get(choix - 1);
+            if (!dvd.getSousTitres().contains(langueChoisie)) {
+                dvd.getSousTitres().add(langueChoisie);
+            } else {
+                System.out.println("Cette langue est déjà sélectionnée.");
+            }
+        } while (true);
         return dvd;
     }
 }
